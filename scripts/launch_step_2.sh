4578a7cb5bf8bed6c380afcbb432d13e6d514694
Ade Lee <alee@redhat.com>
2019-04-12 13:00:24 -0400

    Remove dogtag and hsm bits for Denver 2019

--- scripts/launch_step_2.sh
+++ scripts/launch_step_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT=/root/launch_step_3.sh
-wget -O $SCRIPT https://raw.githubusercontent.com/dave-mccowan/encryption-workshop/master/scripts/launch_step_3.sh
+wget -O $SCRIPT https://raw.githubusercontent.com/vakwetu/encryption-workshop/master/scripts/launch_step_3.sh
 chmod +x $SCRIPT
 . $SCRIPT > /root/setup_go.log
 echo Done!
