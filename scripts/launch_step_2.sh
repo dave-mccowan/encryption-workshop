65204e1143206cb5cca0a89aed0e64afa5ef8834
Ade Lee <alee@redhat.com>
2019-04-12 13:07:02 -0400

    Fix links to point to branch

--- scripts/launch_step_2.sh
+++ scripts/launch_step_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT=/root/launch_step_3.sh
-wget -O $SCRIPT https://raw.githubusercontent.com/vakwetu/encryption-workshop/master/scripts/launch_step_3.sh
+wget -O $SCRIPT https://raw.githubusercontent.com/vakwetu/encryption-workshop/denver_2019/scripts/launch_step_3.sh
 chmod +x $SCRIPT
 . $SCRIPT > /root/setup_go.log
 echo Done!
